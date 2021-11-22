1cf11bedd1eb1d443145a05d577d96399109e51a
Matheus Curvelo <38927923+matheuscurvelo@users.noreply.github.com>
2021-11-22 18:28:46 -0300

    ajuste title public_html/index.php

--- public_html/index.php
+++ public_html/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title><?php echo ucfirst($url[0]); ?></title>
+    <title>Teste Bitix</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
